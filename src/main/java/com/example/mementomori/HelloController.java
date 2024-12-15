7c64b69a140cbfee9b27bf331ddf54d4d0eeea35
Dawid Ciuba <dawidciuba.com@gmail.com>
2024-12-15 21:46:55 +0100

    start

--- src/main/java/com/example/mementomori/HelloController.java
+++ src/main/java/com/example/mementomori/HelloController.java
@@ -1,14 +1,69 @@
 package com.example.mementomori;
 
+import com.dlsc.formsfx.model.structure.IntegerField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+import java.io.File;
+import java.util.Optional;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Button DietaZmienCel;
+    public Button DietaStatystyki;
+    public Button DietaDodajRecznie;
+    public Button DietaHome;
+    public ProgressBar DietaProgres;
+    public Integer cel = 2000;
+    public Integer liczbaKalori = 300;
+    public Text DietaKalorieCel;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    File cssFile = new File("C:\\Users\\dawid\\IdeaProjects\\MementoMori\\src\\main\\css\\progressBar.css");
+
+    void init() {
+        DietaProgres.getStyleClass().add("circular-progress-bar");
+        DietaProgres.getStylesheets().add(cssFile.toURI().toString());
+        DietaProgres.setProgress((double) liczbaKalori / cel);
+        DietaKalorieCel.setText(liczbaKalori + "/" + cel + " kcal");
+
+    }
+
+    public void DietaZmienCel(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Zmiana Celu");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Wprowadź nowy cel:");
+        Optional<String> wynik = dialog.showAndWait();
+        wynik.ifPresent(iloscKalori -> {
+            cel = Integer.parseInt(iloscKalori);
+            DietaProgres.setProgress((double) liczbaKalori / cel);
+            DietaKalorieCel.setText(liczbaKalori + "/" + cel + " kcal");
+        });
+    }
+
+    public void DietaDodajRecznie(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Dodaj kalorie");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Wprowadź ile kcal spożyłeś:");
+        Optional<String> wynik = dialog.showAndWait();
+        wynik.ifPresent(x -> {
+            liczbaKalori += Integer.parseInt(x);
+            DietaProgres.setProgress((double) liczbaKalori / cel);
+            DietaKalorieCel.setText(liczbaKalori + "/" + cel + " kcal");
+        });
+    }
+
+    public void DietaStatystki(ActionEvent actionEvent) {
+    }
+
+    public void ReturnHome(ActionEvent actionEvent) {
+    }
+
+    public void DietaStatystyki(ActionEvent actionEvent) {
     }
 }
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.mementomori {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.mementomori to javafx.fxml;
     exports com.example.mementomori;
