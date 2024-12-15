8434ac8672b909b1ddddddcf50ee4903e7098cac
Dawid Ciuba <dawidciuba.com@gmail.com>
2024-12-15 19:17:26 +0100

    start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.mementomori {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.mementomori to javafx.fxml;
+    exports com.example.mementomori;
+}
