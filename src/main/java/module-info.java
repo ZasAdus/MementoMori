bc662bb14024103f71f5b36ae5c3ae1cf6780dcd
Dawid Ciuba <dawidciuba.com@gmail.com>
2024-12-16 16:15:36 +0100

    Bazy działają idę na przerwę potem połączę to z ekranem logowania

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.mementomori {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires java.sql;
 
     opens com.example.mementomori to javafx.fxml;
     exports com.example.mementomori;
